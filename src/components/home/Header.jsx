f205687191b74ac8c52e1ddde3d468cd0c747b70
tongstar <68733249+tongmon@users.noreply.github.com>
2025-05-10 21:05:10 +0900

    Update header

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,33 +1,32 @@
 import classes from "../home/Header.module.css";
-import profileImage from "../../assets/profile.jpg";
+import profileImage from "../../assets/profile_pixelate.png";
 
 // ref: https://html5up.net/dimension
 
 export function Header() {
   return (
     <header className={classes["welcome-header"]}>
-      <div>
+      {
+        // <img src={profileImage} alt="tongstar's profile image" /> 
+      }
+      <div class="pixel-clip-border">
         <img src={profileImage} alt="tongstar's profile image" />
       </div>
-      <div>
-        <div>
-          <h1>Welcome!</h1>
-          <p>{"This is the blog of tongstar.\nFeel free to look around!"}</p>
-        </div>
-      </div>
+      <h1>Welcome!</h1>
+      <p>{"This is the blog of tongstar.\nFeel free to look around!"}</p>
       <nav>
         <ul>
           <li>
-            <a href="#intro">Intro</a>
+            <a href="#learn">Learn</a>
+          </li>
+          <li>
+            <a href="#life">Life</a>
           </li>
           <li>
             <a href="#work">Work</a>
           </li>
           <li>
             <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
           </li>
         </ul>
       </nav>
