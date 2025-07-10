ac58219a72885f559f264e2b6c7214bf99deeccd
tongmon <tongmon@hanmail.net>
2025-07-10 16:56:56 +0900

    Fix bug on PostGridView

--- src/components/learn/post_grid_view/PostGridView.jsx
+++ src/components/learn/post_grid_view/PostGridView.jsx
@@ -13,9 +13,9 @@ export function PostGridView({ postList }) {
   const location = useLocation();
   const gridTitle = location.pathname.split("/").map(decodeURIComponent).at(-1);
   const [posts, setPosts] = useState([]);
+  const [loadSize, setLoadSize] = useState(12);
   const [hasMore, setHasMore] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [loadSize, setLoadSize] = useState(12);
   const { ref, inView } = useInView({ threshold: 0.5 });
   const maximumLoadSize = 12; // Maximum number of posts to load at once
 
@@ -24,7 +24,6 @@ export function PostGridView({ postList }) {
       posts: [],
       page: 0,
       hasMore: true,
-      loading: false,
       loadSize:
         postList.length < maximumLoadSize ? postList.length : maximumLoadSize,
     });
@@ -40,6 +39,7 @@ export function PostGridView({ postList }) {
       postList.length
     );
     console.log("Loading posts from", start, "to", end);
+    let appendPosts = [];
     for (let i = start; i < end; i++) {
       let postNode = postList[i];
       let postInfo = { date: postNode.date };
@@ -52,73 +52,37 @@ export function PostGridView({ postList }) {
           postInfo["thumbnail"] = thumbnailImg;
         }
       }
-      postGridCache.get(location.pathname).posts.push(postInfo);
-    }
-    setPosts(postGridCache.get(location.pathname).posts); // could be the timing problem
-    if (end - start < maximumLoadSize) {
-      setHasMore(() => {
-        postGridCache.get(location.pathname).hasMore = false;
-        return false;
-      });
-      setLoadSize(() => {
-        postGridCache.get(location.pathname).loadSize = end - start;
-        return postGridCache.get(location.pathname).loadSize;
-      });
+      appendPosts.push(postInfo);
     }
     function sleep(ms) {
       return new Promise((resolve) => setTimeout(resolve, ms));
     }
     await sleep(1000); // Simulate network delay
+    setPosts((prev) => {
+      postGridCache.get(location.pathname).posts.push(...appendPosts);
+      return [...prev, ...appendPosts];
+    });
+    if (end - start < maximumLoadSize) {
+      postGridCache.get(location.pathname).loadSize = end - start;
+      postGridCache.get(location.pathname).hasMore = false;
+      setLoadSize(postGridCache.get(location.pathname).loadSize);
+      setHasMore(postGridCache.get(location.pathname).hasMore);
+    }
     setLoading(false);
   };
 
   useEffect(() => {
-    console.log("PostGridView cache:", postGridCache);
-    setPosts(postGridCache.get(location.pathname).posts);
+    setPosts(postGridCache.get(location.pathname).posts.slice());
     setHasMore(postGridCache.get(location.pathname).hasMore);
     setLoadSize(postGridCache.get(location.pathname).loadSize);
   }, [location.pathname]);
 
-  //useEffect(() => {
-  //  if (!postGridCache.has(location.pathname)) {
-  //    postGridCache.set(location.pathname, {
-  //      posts: [],
-  //      page: 0,
-  //      hasMore: true,
-  //    });
-  //  }
-
-  //  setPosts(postGridCache.get(location.pathname).posts);
-  //  setPage(postGridCache.get(location.pathname).page);
-  //  setHasMore(postGridCache.get(location.pathname).hasMore);
-
-  //  console.log("PostGridView cache:", postGridCache);
-  //}, [location.pathname]);
-
-  // 처음 로딩이 되는 시점에
-  // hasmore true -> useEffect triggered -> loadposts called -> setloading false -> loadposts called
   useEffect(() => {
-    console.log(
-      "Inview",
-      inView,
-      "hasMore",
-      hasMore,
-      "loading",
-      loading,
-      postGridCache
-    );
     if (inView && hasMore && !loading) {
       loadPosts();
       postGridCache.get(location.pathname).page += 1;
     }
   }, [inView, hasMore, loading]);
-
-  // useEffect(() => {
-  //   if ((page === 0 || hasMore) && posts.length < (page + 1) * loadSize) {
-  //     console.log("Loading posts for page:", page);
-  //     loadPosts();
-  //   }
-  // }, [page]);
 
   return (
     <>
